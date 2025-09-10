aaa98d000a9dfd87e71652db689f3914bdef3304
supalukp <supaluk.pun@gmail.com>
2025-09-10 17:31:22 +0200

    fix: copy constructor

--- CPP04/ex00/src/Animal.cpp
+++ CPP04/ex00/src/Animal.cpp
@@ -6,7 +6,7 @@
 /*   By: spunyapr <spunyapr@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/06 11:04:05 by spunyapr          #+#    #+#             */
-/*   Updated: 2025/09/09 13:59:41 by spunyapr         ###   ########.fr       */
+/*   Updated: 2025/09/10 16:31:33 by spunyapr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,13 @@ Animal::Animal(std::string newType) : _type(newType) {
                 << "Animal constructor called" << std::endl;
 }
 
-Animal::Animal(const Animal &other) {
+Animal::Animal(const Animal &other) : _type(other._type) {
     std::cout << "Copy constructor called" << std::endl;
-	*this = other;
 }
 
 Animal& Animal::operator=(const Animal& other) {
     std::cout << "Copy assignment operator called" << std::endl;
-	if (this != &other)
-	{
+	if (this != &other) {
 		this->_type = other._type;
 	}
 	return (*this);
--- CPP04/ex00/src/WrongAnimal.cpp
+++ CPP04/ex00/src/WrongAnimal.cpp
@@ -6,7 +6,7 @@
 /*   By: spunyapr <spunyapr@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/06 14:47:08 by spunyapr          #+#    #+#             */
-/*   Updated: 2025/09/09 14:00:20 by spunyapr         ###   ########.fr       */
+/*   Updated: 2025/09/10 16:31:28 by spunyapr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,13 @@ WrongAnimal::WrongAnimal(std::string newType) : _type(newType) {
                 << "WrongAnimal constructor called" << std::endl;
 }
 
-WrongAnimal::WrongAnimal(const WrongAnimal &other) {
+WrongAnimal::WrongAnimal(const WrongAnimal &other) : _type(other._type) {
     std::cout << "Copy constructor called" << std::endl;
-	*this = other;
 }
 
 WrongAnimal& WrongAnimal::operator=(const WrongAnimal& other) {
     std::cout << "Copy assignment operator called" << std::endl;
-	if (this != &other)
-	{
+	if (this != &other) {
 		this->_type = other._type;
 	}
 	return (*this);
