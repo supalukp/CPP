6f976aab9e293fd5c7cf3fe321a04480d48bfc40
supalukp <supaluk.pun@gmail.com>
2025-11-21 10:28:02 +0100

    fix: test in scope

--- CPP04/ex01/src/main.cpp
+++ CPP04/ex01/src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: spunyapr <spunyapr@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: spunyapr <spunyapr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/06 11:27:49 by spunyapr          #+#    #+#             */
-/*   Updated: 2025/09/17 21:30:33 by spunyapr         ###   ########.fr       */
+/*   Updated: 2025/11/21 10:27:06 by spunyapr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,81 +18,83 @@ int main(void) {
     std::cout << "\n\n==============================" << std::endl;
     std::cout << "        TEST FOR CAT          " << std::endl;
     std::cout << "==============================\n" << std::endl;
-    std::cout << "\n--- Cat Creation ---" << std::endl;
-    Cat cat1;
-    cat1.getBrain()->setIdea(0, "Eat fish");
-    cat1.getBrain()->setIdea(1, "Sleep");
+    {
+        std::cout << "\n--- Cat Creation ---" << std::endl;
+        Cat cat1;
+        cat1.getBrain()->setIdea(0, "Eat fish");
+        cat1.getBrain()->setIdea(1, "Sleep");
 
-    std::cout << "cat1 ideas: " 
-              << cat1.getBrain()->getIdea(0) << ", "
-              << cat1.getBrain()->getIdea(1) << std::endl;
+        std::cout << "cat1 ideas: " 
+                << cat1.getBrain()->getIdea(0) << ", "
+                << cat1.getBrain()->getIdea(1) << std::endl;
 
-    std::cout << "\n--- Copy Constructor ---" << std::endl;
-    Cat cat2(cat1);
-    std::cout << "cat2 ideas: " 
-              << cat2.getBrain()->getIdea(0) << ", "
-              << cat2.getBrain()->getIdea(1) << std::endl;
+        std::cout << "\n--- Copy Constructor ---" << std::endl;
+        Cat cat2(cat1);
+        std::cout << "cat2 ideas: " 
+                << cat2.getBrain()->getIdea(0) << ", "
+                << cat2.getBrain()->getIdea(1) << std::endl;
 
-    std::cout << "\n--- Assignment Operator ---" << std::endl;
-    Cat cat3;
-    cat3 = cat1; 
-    std::cout << "cat3 ideas: " 
-              << cat3.getBrain()->getIdea(0) << ", "
-              << cat3.getBrain()->getIdea(1) << std::endl;
+        std::cout << "\n--- Assignment Operator ---" << std::endl;
+        Cat cat3;
+        cat3 = cat1; 
+        std::cout << "cat3 ideas: " 
+                << cat3.getBrain()->getIdea(0) << ", "
+                << cat3.getBrain()->getIdea(1) << std::endl;
 
-    std::cout << "\n--- Modify Original ---" << std::endl;
-    cat1.getBrain()->setIdea(0, "Play");
-    cat1.getBrain()->setIdea(1, "Run");
+        std::cout << "\n--- Modify Original ---" << std::endl;
+        cat1.getBrain()->setIdea(0, "Play");
+        cat1.getBrain()->setIdea(1, "Run");
 
-    std::cout << "cat1 ideas: " 
-              << cat1.getBrain()->getIdea(0) << ", "
-              << cat1.getBrain()->getIdea(1) << std::endl;
-    std::cout << "cat2 ideas: " 
-              << cat2.getBrain()->getIdea(0) << ", "
-              << cat2.getBrain()->getIdea(1) << std::endl;
-    std::cout << "cat3 ideas: " 
-              << cat3.getBrain()->getIdea(0) << ", "
-              << cat3.getBrain()->getIdea(1) << std::endl;
-
+        std::cout << "cat1 ideas: " 
+                << cat1.getBrain()->getIdea(0) << ", "
+                << cat1.getBrain()->getIdea(1) << std::endl;
+        std::cout << "cat2 ideas: " 
+                << cat2.getBrain()->getIdea(0) << ", "
+                << cat2.getBrain()->getIdea(1) << std::endl;
+        std::cout << "cat3 ideas: " 
+                << cat3.getBrain()->getIdea(0) << ", "
+                << cat3.getBrain()->getIdea(1) << std::endl;
+    }
     std::cout << "\n\n==============================" << std::endl;
     std::cout << "        TEST FOR DOG          " << std::endl;
     std::cout << "==============================\n" << std::endl;
-    std::cout << "\n--- Dog Creation ---" << std::endl;
-    Dog dog1;
-    dog1.getBrain()->setIdea(0, "Bark");
-    dog1.getBrain()->setIdea(1, "Eat");
+    {
+        std::cout << "\n--- Dog Creation ---" << std::endl;
+        Dog dog1;
+        dog1.getBrain()->setIdea(0, "Bark");
+        dog1.getBrain()->setIdea(1, "Eat");
 
-    std::cout << "dog1 ideas: " 
-            << dog1.getBrain()->getIdea(0) << ", "
-            << dog1.getBrain()->getIdea(1) << std::endl;
+        std::cout << "dog1 ideas: " 
+                << dog1.getBrain()->getIdea(0) << ", "
+                << dog1.getBrain()->getIdea(1) << std::endl;
 
-    std::cout << "\n--- Copy Constructor ---" << std::endl;
-    Dog dog2(dog1);
-    std::cout << "dog2 ideas: " 
-            << dog2.getBrain()->getIdea(0) << ", "
-            << dog2.getBrain()->getIdea(1) << std::endl;
+        std::cout << "\n--- Copy Constructor ---" << std::endl;
+        Dog dog2(dog1);
+        std::cout << "dog2 ideas: " 
+                << dog2.getBrain()->getIdea(0) << ", "
+                << dog2.getBrain()->getIdea(1) << std::endl;
 
-    std::cout << "\n--- Assignment Operator ---" << std::endl;
-    Dog dog3;
-    dog3 = dog1;
-    std::cout << "dog3 ideas: " 
-            << dog3.getBrain()->getIdea(0) << ", "
-            << dog3.getBrain()->getIdea(1) << std::endl;
+        std::cout << "\n--- Assignment Operator ---" << std::endl;
+        Dog dog3;
+        dog3 = dog1;
+        std::cout << "dog3 ideas: " 
+                << dog3.getBrain()->getIdea(0) << ", "
+                << dog3.getBrain()->getIdea(1) << std::endl;
 
-    std::cout << "\n--- Modify Original ---" << std::endl;
-    dog1.getBrain()->setIdea(0, "Run");
-    dog1.getBrain()->setIdea(1, "Play");
+        std::cout << "\n--- Modify Original ---" << std::endl;
+        dog1.getBrain()->setIdea(0, "Run");
+        dog1.getBrain()->setIdea(1, "Play");
 
-    std::cout << "dog1 ideas: " 
-            << dog1.getBrain()->getIdea(0) << ", "
-            << dog1.getBrain()->getIdea(1) << std::endl;
-    std::cout << "dog2 ideas: " 
-            << dog2.getBrain()->getIdea(0) << ", "
-            << dog2.getBrain()->getIdea(1) << std::endl;
-    std::cout << "dog3 ideas: " 
-            << dog3.getBrain()->getIdea(0) << ", "
-            << dog3.getBrain()->getIdea(1) << std::endl;
-
+        std::cout << "dog1 ideas: " 
+                << dog1.getBrain()->getIdea(0) << ", "
+                << dog1.getBrain()->getIdea(1) << std::endl;
+        std::cout << "dog2 ideas: " 
+                << dog2.getBrain()->getIdea(0) << ", "
+                << dog2.getBrain()->getIdea(1) << std::endl;
+        std::cout << "dog3 ideas: " 
+                << dog3.getBrain()->getIdea(0) << ", "
+                << dog3.getBrain()->getIdea(1) << std::endl;
+        }
     std::cout << "\n\n==============================" << std::endl;
     std::cout << "     TEST FOR Polymorphism    " << std::endl;
     std::cout << "==============================\n" << std::endl;
@@ -115,7 +117,8 @@ int main(void) {
     std::cout << "\n--- Clean up ---" << std::endl;
     for (int i = 0; i < 10; i++)
         delete animals[i];
-
+    std::cout << "\n--- End Clean up ---" << std::endl;
+    
     return (0);
 }
 
