e6094d3d1b53c40cb8cab1037f0508a321894a88
supalukp <supaluk.pun@gmail.com>
2025-12-01 14:58:33 +0100

    fix: add signForm test in main

--- CPP05/ex01/src/main.cpp
+++ CPP05/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: spunyapr <spunyapr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 16:03:21 by spunyapr          #+#    #+#             */
-/*   Updated: 2025/12/01 13:31:15 by spunyapr         ###   ########.fr       */
+/*   Updated: 2025/12/01 14:57:44 by spunyapr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -112,5 +112,62 @@ int main(void)
         }
         std::cout << b << std::endl;
     }
+
+    std::cout << "\n==============================" << std::endl;
+    std::cout << "      TEST signForm()     " << std::endl;
+    std::cout << "==============================" << std::endl;
+    {
+        std::cout << "\n--- Test: bureaucrat´s grade is high enough ---" << std::endl;
+        Form b("B", 50, 50);
+        Bureaucrat a("Bureaucrat A", 50);
+        std::cout << b << std::endl;
+        std::cout << a << std::endl;
+
+        try
+        {
+            a.signForm(b);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << "Catch Exception signForm(): " << e.what() << std::endl;
+        }
+        std::cout << b << std::endl;
+    }
+    {
+        std::cout << "\n--- Test: bureaucrat´s grade is equal ---" << std::endl;
+        Form b("B", 50, 50);
+        Bureaucrat a("Bureaucrat A", 1);
+        std::cout << b << std::endl;
+        std::cout << a << std::endl;
+
+        try
+        {
+            a.signForm(b);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << "Catch Exception signForm(): " << e.what() << std::endl;
+        }
+        std::cout << b << std::endl;
+    }
+    {
+        std::cout << "\n--- Test: bureaucrat´s grade is lower ---" << std::endl;
+        Form b("B", 50, 50);
+        Bureaucrat a("Bureaucrat A", 150);
+        std::cout << b << std::endl;
+        std::cout << a << std::endl;
+
+        try
+        {
+            a.signForm(b);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << "Catch Exception signForm(): " << e.what() << std::endl;
+        }
+        std::cout << b << std::endl;
+    }
+    std::cout << "\n--- End Test ---" << std::endl;
+    
     return (0);
 }
