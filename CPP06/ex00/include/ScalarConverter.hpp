05f5a0adaa31c3df9b83da4a4fabd788f65a4198
supalukp <supaluk.pun@gmail.com>
2025-12-03 15:31:05 +0100

    add: ex00

---
+++ CPP06/ex00/include/ScalarConverter.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spunyapr <spunyapr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/02 15:42:53 by spunyapr          #+#    #+#             */
+/*   Updated: 2025/12/03 14:42:57 by spunyapr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALAR_CONVERTER_HPP
+#define SCALAR_CONVERTER_HPP
+
+#include <iostream>
+
+class ScalarConverter
+{
+    public:
+        static void convert(std::string input);
+};
+
+#endif
