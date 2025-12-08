ce70f6a031770a40c1b863488013095060968e60
supalukp <supaluk.pun@gmail.com>
2025-12-08 16:09:16 +0100

    add: non void return type template

--- CPP07/ex01/include/iter.hpp
+++ CPP07/ex01/include/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: spunyapr <spunyapr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 14:17:18 by spunyapr          #+#    #+#             */
-/*   Updated: 2025/12/08 15:51:01 by spunyapr         ###   ########.fr       */
+/*   Updated: 2025/12/08 16:07:54 by spunyapr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,35 @@
 #include <iostream>
 
 template <typename T>
-void iter(T *array, const size_t lenght, void (*func)(T &))
+void iter(T *array, const size_t length, void (*func)(T&))
 {
-    for(size_t i = 0; i < lenght; i++)
+    for(size_t i = 0; i < length; i++)
     {
         func(array[i]);
     }
 }
 
 template <typename T>
-void iter(const T *array, const size_t lenght, void (*func)(const T &))
+void iter(const T *array, const size_t length, void (*func)(const T&))
 {
-    for(size_t i = 0; i < lenght; i++)
+    for(size_t i = 0; i < length; i++)
     {
         func(array[i]);
     }
+}
+
+template <typename T, typename F>
+void iter(T *array, const size_t length, F (*func)(T&))
+{
+    for(size_t i = 0; i < length; i++)
+    {
+        std::cout << func(array[i]) << " ";
+    }
+}
+
+int minusOne(int &x)
+{
+    return (x - 1);
 }
 
 template <typename T>
--- CPP07/ex01/src/main.cpp
+++ CPP07/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: spunyapr <spunyapr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 14:40:28 by spunyapr          #+#    #+#             */
-/*   Updated: 2025/12/08 15:52:31 by spunyapr         ###   ########.fr       */
+/*   Updated: 2025/12/08 16:08:33 by spunyapr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,6 +101,22 @@ int main(void)
         iter(&str[0], len, &printValue);
         std::cout << std::endl;
         
+    }
+    std::cout << "\n==============================" << std::endl;
+    std::cout << "  TEST: non return void function    " << std::endl;
+    std::cout << "==============================" << std::endl;
+    {
+        std::cout << "\n--- Test: int array ---" << std::endl;
+        int array[] = {1,2,3,4,5,6,7,8,9,10};
+        const int len = 10;
+        std::cout << "Original: " << std::endl;
+        for (int i = 0; i < len; i++)
+            std::cout << array[i] << " ";
+        std::cout << std::endl;
+        
+        std::cout << "call iter with ft minusOne: " << std::endl;
+        iter(array, len, &minusOne);
+        std::cout << std::endl;
     }
     return (0);
 }
