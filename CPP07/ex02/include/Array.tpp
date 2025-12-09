dc8331e20d6ad35d2f4f89d72a2c33514f1643b7
supalukp <supaluk.pun@gmail.com>
2025-12-09 17:14:29 +0100

    add: main test for type int

--- CPP07/ex02/include/Array.tpp
+++ CPP07/ex02/include/Array.tpp
@@ -6,7 +6,7 @@
 /*   By: spunyapr <spunyapr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 16:34:07 by spunyapr          #+#    #+#             */
-/*   Updated: 2025/12/09 14:02:34 by spunyapr         ###   ########.fr       */
+/*   Updated: 2025/12/09 17:01:45 by spunyapr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,28 @@ template <typename T>
 Array<T>::Array(void)
     : _array(new T[0]), _size(0)
 {
+    std::cout << "Default constructor called" << std::endl;
 }
 
 template <typename T>
 Array<T>::Array(unsigned int n) : _array(new T[n]), _size(n)
 {
+    for(unsigned int i = 0; i < _size; i++) 
+    {
+        _array[i] = 0;
+    }
+    std::cout << "Constructor called with parameter" << std::endl;
 }
 
 template <typename T>
-Array<T>::Array(const Array &other)
+Array<T>::Array(const Array &other) : _array(new T[other._size]), _size(other._size)
 {
-    for(int i = 0; i < other.size(); i++) 
+    for(unsigned int i = 0; i < other.size(); i++) 
     {
         _array[i] = other._array[i];
     }
+	std::cout << "Copy constructor called" << std::endl;
+
 }
 
 template <typename T>
@@ -35,11 +43,12 @@ Array<T>& Array<T>::operator=(const Array& other)
 {
     if (this != &other) 
     {
-        for(int i = 0; i < other.size(); i++) 
+        for(unsigned int i = 0; i < other.size(); i++) 
         {
             _array[i] = other._array[i];
         }
     }
+	std::cout << "Copy assignment operator called" << std::endl;
     return (*this);
 }
 
@@ -47,7 +56,7 @@ template <typename T>
 T& Array<T>::operator[](unsigned int idx)
 {
     if (idx < 0 || idx >= _size)
-        throw std::out_of_range("out of range");
+        throw std::out_of_range("index is out of range");
         
     return (_array[idx]);
 }
@@ -56,6 +65,7 @@ template <typename T>
 Array<T>::~Array(void)
 {
     delete [] _array;
+    std::cout << "Destructor called" << std::endl;
 }
 
 template <typename T>
--- CPP07/ex02/src/main.cpp
+++ CPP07/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: spunyapr <spunyapr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 16:11:21 by spunyapr          #+#    #+#             */
-/*   Updated: 2025/12/09 14:00:43 by spunyapr         ###   ########.fr       */
+/*   Updated: 2025/12/09 17:14:03 by spunyapr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,125 @@
 
 int main (void)
 {
-    Array <int>int_array(2);
-    std::cout << int_array.size() << std::endl;
-    try 
+    std::cout << "\n==============================" << std::endl;
+    std::cout << "         Type : INT   " << std::endl;
+    std::cout << "==============================" << std::endl;
     {
-        std::cout << "array[1] = "<< int_array[1] << std::endl;
+        // Test default constructors
+        std::cout << "\n---- Test default constructors ----" << std::endl;
+        Array <int>a;
+        std::cout << "size of a = " << a.size() << std::endl;
+
+        //  Test parameterized constructors
+        std::cout << "\n---- Test parameterized constructors ----" << std::endl;
+        Array <int>b(5);
+        std::cout << "size of b = " << b.size() << std::endl;
+        for (unsigned int i = 0; i < b.size(); i++)
+            b[i] = i+1;
+        for (unsigned int i = 0; i < b.size(); i++)
+            std::cout << b[i] << " ";
+        std::cout << "\n";
+        
+        // Test copy constructors
+        std::cout << "\n---- Test copy constructors ----" << std::endl;
+        Array <int>copy(b);
+        std::cout << "size of copy from b = " << copy.size() << std::endl;
+        for (unsigned int i = 0; i < copy.size(); i++)
+            std::cout << copy[i] << " ";
+        std::cout << "\n";
+
+        // Test assignment operators
+        std::cout << "\n---- Test assignment operators ----" << std::endl;
+        
+        std::cout << "\n*** Copy from smaller array ***" << std::endl;
+        Array <int>big(10);
+        for (unsigned int i = 0; i < big.size(); i++)
+            big[i] = 10 - i;
+        std::cout << "Original big: ";
+        for (unsigned int i = 0; i < big.size(); i++)
+            std::cout << big[i] << " ";
+        std::cout << "\n";
+        std::cout << "Original b: ";
+        for (unsigned int i = 0; i < b.size(); i++)
+            std::cout << b[i] << " ";
+        std::cout << "\n";
+        
+        big = b;
+        std::cout << "After copy from b: ";
+        for (unsigned int i = 0; i < big.size(); i++)
+            std::cout << big[i] << " ";
+        std::cout << "\n";
+        
+        std::cout << "---------------------------------" << std::endl;
+        
+        std::cout << "\n*** Copy from bigger array ***" << std::endl;
+        Array <int>small(2);
+        for (unsigned int i = 0; i < small.size(); i++)
+            small[i] = 10 - i;
+        std::cout << "Original small: ";
+        for (unsigned int i = 0; i < small.size(); i++)
+            std::cout << small[i] << " ";
+        std::cout << "\n";
+        std::cout << "Original b: ";
+        for (unsigned int i = 0; i < b.size(); i++)
+            std::cout << b[i] << " ";
+        std::cout << "\n";
+        
+        small = b;
+        std::cout << "After copy from b: ";
+        for (unsigned int i = 0; i < small.size(); i++)
+            std::cout << small[i] << " ";
+        std::cout << "\n";
+
+        std::cout << "\n---- Test subscript operator[] ----" << std::endl;
+        std::cout << "Original b: ";
+        for (unsigned int i = 0; i < b.size(); i++)
+            std::cout << b[i] << " ";
+        std::cout << "\n";
+        std::cout << " < Try to modify 1st element to 100 >" << std::endl;
+        try
+        {
+            b[0] = 100;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << "Catch Exception: " << e.what() << std::endl;
+        }
+        std::cout << "After modify b: ";
+        for (unsigned int i = 0; i < b.size(); i++)
+            std::cout << b[i] << " ";
+        std::cout << "\n";
+
+        std::cout << "\n---- Test subscript operator[] ----" << std::endl;
+        std::cout << "size of b = " << b.size() << std::endl;
+        std::cout << " < Try to modify index out of range >" << std::endl;
+        try
+        {
+            b[b.size()] = 100;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << "Catch Exception: " << e.what() << std::endl;
+        }
+        std::cout << "After failed to modify b: ";
+        for (unsigned int i = 0; i < b.size(); i++)
+            std::cout << b[i] << " ";
+        std::cout << "\n";
+
+        std::cout << "\n---- Test size() ----" << std::endl;
+        std::cout << "Elements in b: ";
+        for (unsigned int i = 0; i < b.size(); i++)
+            std::cout << b[i] << " ";
+        std::cout << "\n";
+        std::cout << "size of b = " << b.size() << std::endl;
+
+        std::cout << "\n----------- Destructor ------------" << std::endl;
     }
-    catch(const std::exception& e)
     {
-        std::cerr << "Catch Exception: " << e.what() << std::endl;
+        
     }
-    
-    Array <int>try_array;
-    std::cout << try_array.size() << std::endl;
-    
-    int *a = new int(20);
-    std::cout << *a << std::endl;
-    delete a;
+
+    std::cout << "\n----------- End test ------------" << std::endl;
     
     return (0);
 }
