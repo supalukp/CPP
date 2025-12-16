22cd18aa9b873ebc5a6ea4e686e5dde70742247f
supalukp <supaluk.pun@gmail.com>
2025-12-16 11:01:37 +0100

    fix: add algorithm in easyfind() + test in main

--- CPP08/ex00/include/easyfind.hpp
+++ CPP08/ex00/include/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: spunyapr <spunyapr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 09:23:25 by spunyapr          #+#    #+#             */
-/*   Updated: 2025/12/15 15:43:34 by spunyapr         ###   ########.fr       */
+/*   Updated: 2025/12/16 10:59:53 by spunyapr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include <iostream>
 #include <exception>
+#include <algorithm>
 
 class NotFound : public std::exception
 {
@@ -25,16 +26,12 @@ class NotFound : public std::exception
         }
 };
 
-
 template <typename T>
-int easyfind(T &container, int value)
+int easyfind(T &c, int value)
 {
-    for(typename Container::iterator it = c.begin(); it != c.end(); it++)
-    {
-        if (*it == value)
-            return (value);
-    }
-    throw NotFound();
+    if ((std::find(c.begin(), c.end(), value)) == c.end())
+        throw NotFound();
+    return (value);
 }
 
 #endif
--- CPP08/ex00/src/main.cpp
+++ CPP08/ex00/src/main.cpp
@@ -6,23 +6,18 @@
 /*   By: spunyapr <spunyapr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 09:23:22 by spunyapr          #+#    #+#             */
-/*   Updated: 2025/12/15 15:41:33 by spunyapr         ###   ########.fr       */
+/*   Updated: 2025/12/16 11:00:36 by spunyapr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 #include <vector>
-#include <array>
-#include <iostream>
+#include <deque>
+#include <list>
+#include <set>
 
 int main(void)
 {
-    std::cout << "\n==============================" << std::endl;
-    std::cout << "        TEST: Array     " << std::endl;
-    std::cout << "==============================" << std::endl;
-    {
-        
-    }
     std::cout << "\n==============================" << std::endl;
     std::cout << "        TEST: Vector    " << std::endl;
     std::cout << "==============================" << std::endl;
@@ -34,7 +29,7 @@ int main(void)
         vec.push_back(40);
         vec.push_back(50);
 
-        std::cout << "Element inside vetor = ";
+        std::cout << "Element inside vector = ";
         for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
             std::cout << *it << " ";
         std::cout << std::endl;
@@ -45,7 +40,7 @@ int main(void)
         {
             std::cout << "Try to find " << x << std::endl;
             if (easyfind(vec, x) == x)
-                std::cout << "easyfind() found x" << std::endl;
+                std::cout << "easyfind() found " << x  << std::endl;
         }
         catch(const std::exception& e)
         {
@@ -57,13 +52,143 @@ int main(void)
         {
             std::cout << "Try to find " << x << std::endl;
             if (easyfind(vec, x) == x)
-                std::cout << "easyfind() found x" << std::endl;
+                std::cout << "easyfind() found " << x  << std::endl;
         }
         catch(const std::exception& e)
         {
             std::cerr << e.what() << '\n';
         }
     }
-    
+    std::cout << "\n==============================" << std::endl;
+    std::cout << "        TEST: Deque    " << std::endl;
+    std::cout << "==============================" << std::endl;
+    {
+        std::deque<int> deq;
+        deq.push_back(1);
+        deq.push_back(2);
+        deq.push_back(3);
+        deq.push_back(4);
+        deq.push_back(5);
+
+        std::cout << "Element inside deque = ";
+        for (std::deque<int>::iterator it = deq.begin(); it != deq.end(); ++it)
+            std::cout << *it << " ";
+        std::cout << std::endl;
+        
+        std::cout << "\n--- Test: Findable ---" << std::endl;
+        int x = 3;
+        try
+        {
+            std::cout << "Try to find " << x << std::endl;
+            if (easyfind(deq, x) == x)
+                std::cout << "easyfind() found " << x  << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        std::cout << "\n--- Test: Not findable ---" << std::endl;
+        x = 6;
+        try
+        {
+            std::cout << "Try to find " << x << std::endl;
+            if (easyfind(deq, x) == x)
+                std::cout << "easyfind() found " << x  << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+    }
+    std::cout << "\n==============================" << std::endl;
+    std::cout << "        TEST: List    " << std::endl;
+    std::cout << "==============================" << std::endl;
+    {
+        std::list<int> list;
+        list.push_back(111);
+        list.push_back(12565);
+        list.push_back(3544);
+        list.push_back(45484);
+        list.push_back(44515);
+
+        std::cout << "Element inside list = ";
+        for (std::list<int>::iterator it = list.begin(); it != list.end(); ++it)
+            std::cout << *it << " ";
+        std::cout << std::endl;
+        
+        std::cout << "\n--- Test: Findable ---" << std::endl;
+        int x = 44515;
+        try
+        {
+            std::cout << "Try to find " << x << std::endl;
+            if (easyfind(list, x) == x)
+                std::cout << "easyfind() found " << x  << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        std::cout << "\n--- Delete last element" << std::endl;
+        list.pop_back();
+        std::cout << "Element inside list = ";
+        for (std::list<int>::iterator it = list.begin(); it != list.end(); ++it)
+            std::cout << *it << " ";
+        std::cout << std::endl;
+        std::cout << "\n--- Test: Not findable ---" << std::endl;
+        x = 44515;
+        try
+        {
+            std::cout << "Try to find " << x << std::endl;
+            if (easyfind(list, x) == x)
+                std::cout << "easyfind() found " << x  << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+    }
+    std::cout << "\n==============================" << std::endl;
+    std::cout << "        TEST: Set    " << std::endl;
+    std::cout << "==============================" << std::endl;
+    {
+        std::set<int> set;
+        set.insert(0);
+        set.insert(5);
+        set.insert(8);
+        set.insert(1);
+        set.insert(20);
+
+        std::cout << "Element inside set = ";
+        for (std::set<int>::iterator it = set.begin(); it != set.end(); ++it)
+            std::cout << *it << " ";
+        std::cout << std::endl;
+        
+        std::cout << "\n--- Test: Findable ---" << std::endl;
+        int x = 5;
+        try
+        {
+            std::cout << "Try to find " << x << std::endl;
+            if (easyfind(set, x) == x)
+                std::cout << "easyfind() found " << x  << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        std::cout << "\n--- Test: Not findable ---" << std::endl;
+        x = 10;
+        try
+        {
+            std::cout << "Try to find " << x << std::endl;
+            if (easyfind(set, x) == x)
+                std::cout << "easyfind() found " << x  << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+    }
+        std::cout << "\n--- End of the test ---" << std::endl;
+
     return (0);
 }
