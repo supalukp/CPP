fca820c4bf9c78ec9b2ac5dcdc5671f36829f42d
supalukp <supaluk.pun@gmail.com>
2025-12-17 17:34:06 +0100

    feat: ex02

---
+++ CPP08/ex02/include/MutantStack.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spunyapr <spunyapr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/17 13:23:10 by spunyapr          #+#    #+#             */
+/*   Updated: 2025/12/17 14:37:58 by spunyapr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+    
+};
+
+#endif
